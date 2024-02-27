b074a13566dab6354f8df48a61ad52e69dde4682
Raja Kumar Bisi <143925079+Kumarraja25@users.noreply.github.com>
2024-02-27 18:42:06 +0000

    pythagoras_triplet

--- pythagoras_triplet.c
+++ pythagoras_triplet.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
+    int arr[]={3,10,8,5,4,6,13,12}; 
     int count=0;
     int size=sizeof(arr)/sizeof(arr[0]);
     //printf("%d",size);
     int sum=0;
     for(int i=0;i<size;i++){
         for(int j=i+1;j<size;j++){
-            sum=arr[i]*arr[i]+arr[j]*arr[j];
-            for(int k=j+1;k<size;k++){
-                      if(sum==arr[k]*arr[k]){
+            for(int k=0;k<size;k++){
+                      if(arr[i]*arr[i]+arr[j]*arr[j]==arr[k]*arr[k]){
                          count++;
                          printf("%d,",arr[i]);
                          printf("%d,",arr[j]);
